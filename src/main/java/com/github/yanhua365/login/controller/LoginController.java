a7d5336d14edbc248b526b270e97ed98234aa56b
yanhua365 <yanhua365@gmail.com>
2013-11-25 11:44:58 +0800

    增加log4j的配置

--- src/main/java/com/github/yanhua365/login/controller/LoginController.java
+++ src/main/java/com/github/yanhua365/login/controller/LoginController.java
@@ -2,6 +2,9 @@ package com.github.yanhua365.login.controller;
 
 import com.github.yanhua365.sample.domain.Person;
 import com.github.yanhua365.sample.service.PersonService;
+import org.apache.commons.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,8 +17,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class LoginController {
 
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @RequestMapping("/")
     public String index(ModelMap modelMap){
+        //下面的logger仅仅是测试slf4j和log4j的配置
+        logger.info("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        logger.debug("begin request {},the view tpl is {}","/","login/index");
         return "login/index";
     }
 
